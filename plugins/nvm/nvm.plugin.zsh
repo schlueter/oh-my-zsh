85f34c7205954b5732bade494a9b6262217a3f3e
Schlueter <bs@schlueter.blue>
2015-05-31 23:24:20 -0400

    Add nvm_prompt_info to nvm plugi

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,3 +1,35 @@
 # The addition 'nvm install' attempts in ~/.profile
 
 [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
+
+_homebrew-installed() {
+  type brew &> /dev/null
+}
+
+_nvm-from-homebrew-installed() {
+  brew --prefix nvm &> /dev/null
+}
+
+FOUND_NVM=0
+nvmdirs=("$HOME/.nvm" "/usr/local/nvm" "/opt/nvm" "/usr/local/opt/nvm")
+if _homebrew-installed && _nvm-from-homebrew-installed; then
+    nvmdirs=($(brew --prefix nvm) "${nvmdirs[@]}")
+fi
+
+for nvmdir in "${nvmdirs[@]}" ; do
+    if [ -d $nvmdir -a $FOUND_NVM -eq 0 ] ; then
+        FOUND_NVM=1
+        export NVM_ROOT=$nvmdir
+        export PATH=${nvmdir}/bin:$PATH
+        source ${NVM_ROOT}/nvm.sh
+
+        function nvm_prompt_info() {
+            echo "$(nvm current)"
+        }
+    fi
+done
+unset nvmdir
+
+if [ $FOUND_NVM -eq 0 ] ; then
+    function nvm_prompt_info() { echo "system: $(node -v 2>&1)" }
+fi
